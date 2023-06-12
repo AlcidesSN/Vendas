98bca456a6fbc4a1b3fda403499ff19f4a3b0468
AlcidesSN <alcidessandoli@gmail.com>
2023-06-12 17:53:32 -0300

    Adicionado consulta de vendas

--- index.php
+++ index.php
@@ -14,13 +14,72 @@
       or die('Erro ao conectar ao banco de dados.');
 
     //Obtem os dados do banco de dados
-    $query = "SELECT * FROM client;";//Codigo em SQL
+    $query = "SELECT vendas.id, products.name AS 'product', vendas.amount, products.price, products.price*vendas.amount AS 'total', salemans.name AS 'saleman',client.name AS 'client', vendas.date, vendas.total AS 'total_venda' FROM vendas, products,salemans,client WHERE products.id = vendas.id_product AND
+	  salemans.id = vendas.id_saleman AND
+	  client.id = vendas.id_client ORDER by vendas.id ASC;";//Codigo em SQL
 
 
     $data = mysqli_query($bdc, $query)
     or die('Erro ao consultar o banco de dados.');
 
-    
+
+    $i = true;
+    while($row = mysqli_fetch_array($data)){
+     
+      if(isset($sep)){
+        if(($sep !=  $row['date'] )){
+          echo '</table>';
+          echo '</tbody>';
+          echo '<hr>';
+
+          echo '<p><strong>Vendedor: </strong>' . $row['saleman'] . 
+          ' <strong>Cliente: </strong>' . $row['client'] . '<br>' .
+          '<strong>Total: </strong>' . $row['total_venda'] . '</p>';
+
+          echo '<table>';
+          echo '<thead>';
+          echo '<tr>';
+          echo '<td><strong>Produto</strong></td>';
+          echo '<td><strong>Valor</strong></td>';
+          echo '<td><strong>Quantidade</strong></td>';
+          echo '<td><strong>Total</strong></td>';
+          echo '</tr>';
+          echo '</thead>';
+          echo '<tbody>';
+        }
+      }
+      if($i==true){
+      
+        echo '<p><strong>Vendedor: </strong>' . $row['saleman'] . 
+        ' <strong>Cliente: </strong>' . $row['client'] . '<br>' .
+        '<strong>Total: </strong>' . $row['total_venda'] . '</p>';
+
+        echo '<table>';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<td><strong>Produto</strong></td>';
+        echo '<td><strong>Valor</strong></td>';
+        echo '<td><strong>Quantidade</strong></td>';
+        echo '<td><strong>Total</strong></td>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        $sep = $row['date'] ;
+      }
+
+      echo '<tr>';
+      echo '<td>' . $row['product'] . '</td>';
+      echo '<td>' . $row['price'] . '</td>';
+      echo '<td>' . $row['amount'] . '</td>';
+      echo '<td>' . $row['total'] . '</td>';
+      echo '</tr>';
+
+
+     
+      $sep = $row['date'];
+      $i = false;
+    }
+    echo '</table>';
     mysqli_close($bdc);
     ?>
 </body>
